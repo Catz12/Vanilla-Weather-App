bfa4ab163ce44ce7bb5e8869c27942119e08b44c
Catz12 <129672729+Catz12@users.noreply.github.com>
2023-08-15 17:40:17 +0200

    Showing current Tempereture JS - AXIOS

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
-alert("Hello!");
+function displyTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "3e326e9ac90aod9b4bff7t16c703f460";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Zurich&key=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displyTemperature);
